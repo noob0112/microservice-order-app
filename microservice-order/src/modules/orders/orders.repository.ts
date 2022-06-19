5d280cd2439ea11b25420f1056e7b97de3b8a1ee
Nguyen Viet Hoang <hoang011220@gmail.com>
2022-06-19 09:24:59 +0700

    add function updateStateOrderbyId

--- microservice-order/src/modules/orders/orders.repository.ts
+++ microservice-order/src/modules/orders/orders.repository.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Repository } from '../../common/repository/mongo.repository';
+import { STATES_ORDER_ENUM } from './constants/orders.constant';
 import { Order, OrderDocument } from './models/orders.schema';
 
 @Injectable()
@@ -11,5 +12,61 @@ export class OrderRepository extends Repository {
     @InjectModel(Order.name) private readonly orderModel: Model<OrderDocument>,
   ) {
     super(orderModel);
+  }
+
+  updateStateOrderbyid(id, stateUpdate) {
+    return new Promise((resole, reject) => {
+      this.findById(id).then((order) => {
+        if (!order) {
+          reject({
+            message: 'Order is incorrect',
+            statusCode: HttpStatus.BAD_REQUEST,
+          });
+        }
+
+        if (!this.switchStateOrder(order, stateUpdate))
+          reject({
+            message: 'BAD_REQUEST',
+            statusCode: HttpStatus.BAD_REQUEST,
+          });
+
+        order.state = stateUpdate;
+        resole(order.save());
+      });
+    });
+  }
+
+  private switchStateOrder(order, stateOrderUpdate) {
+    switch (order.state) {
+      case STATES_ORDER_ENUM.CREATED:
+        if (
+          stateOrderUpdate !== STATES_ORDER_ENUM.CONFIRMED &&
+          stateOrderUpdate !== STATES_ORDER_ENUM.CANCELED
+        ) {
+          return false;
+        }
+
+        return true;
+
+      case STATES_ORDER_ENUM.CONFIRMED:
+        if (
+          stateOrderUpdate !== STATES_ORDER_ENUM.DELIVERED &&
+          stateOrderUpdate !== STATES_ORDER_ENUM.CANCELED
+        ) {
+          return false;
+        }
+
+        return true;
+        break;
+
+      case STATES_ORDER_ENUM.DELIVERED:
+        return false;
+
+      case STATES_ORDER_ENUM.CANCELED:
+        return false;
+
+      default:
+        break;
+    }
   }
 }
